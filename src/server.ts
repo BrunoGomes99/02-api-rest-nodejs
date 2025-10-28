6761d00fc29fe0e84a352b5aca890450347a5fd4
Bruno Gomes <brunogomess7@gmail.com>
2025-10-28 17:47:55 -0300

    Setting host on server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import { app } from './app.ts'
 import { env } from './env/index.ts'
 
+const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost'
+
 app
   .listen({
+    host,
     port: env.PORT,
   })
   .then(() => {
